b81b00210ced54f510a13cafeacf320c786e2da4
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2021-06-15 12:56:45 +0200

    Clean-up and restore skipped tests

--- __tests__/client.js
+++ __tests__/client.js
@@ -12,38 +12,50 @@ back.fixtures = `${__dirname}/nockFixtures`;
 
 back.setMode('lockdown');
 
-describe.skip('missing arguments', () => {
+describe('missing arguments', () => {
 	test('no arguments', () => {
-		expect(new Shutter()).toThrowErrorMatchingInlineSnapshot(
-			'"Parameter \\"email\\" is required"'
+		function func() {
+			return new Shutter();
+		}
+
+		expect(func).toThrowErrorMatchingInlineSnapshot(
+			'"Cannot destructure property \'email\' of \'_ref\' as it is undefined."'
 		);
 	});
 
 	test('no email', () => {
-		expect(
-			new Shutter({
-				password: 'your_pass',
-			})
-		).toThrowErrorMatchingInlineSnapshot('"Parameter \\"email\\" is required"');
+		function func() {
+			return new Shutter({});
+		}
+
+		expect(func).toThrowErrorMatchingInlineSnapshot(
+			'"Parameter \\"email\\" is required"'
+		);
 	});
 
 	test('email, no password', () => {
-		expect(
-			new Shutter({
+		function func() {
+			return new Shutter({
 				email: 'example@mail.com',
-			})
-		).toThrowErrorMatchingInlineSnapshot(
+			});
+		}
+
+		expect(func).toThrowErrorMatchingInlineSnapshot(
 			'"Parameter \\"password\\" is required"'
 		);
 	});
 
 	test('email and password, no uri', () => {
-		expect(
-			new Shutter({
+		function func() {
+			return new Shutter({
 				email: 'example@mail.com',
 				password: 'your_pass',
-			})
-		).toThrowErrorMatchingInlineSnapshot('"Parameter \\"uri\\" is required"');
+			});
+		}
+
+		expect(func).toThrowErrorMatchingInlineSnapshot(
+			'"Parameter \\"uri\\" is required"'
+		);
 	});
 });
 
@@ -73,57 +85,55 @@ describe('full config', () => {
 				reason: 'String',
 			};
 
-			return back('Events_addEvent.json')
-				.then(({ nockDone }) => shutter.events.create(event).finally(nockDone))
-				.then(response => {
-					expect(response).toMatchInlineSnapshot(`
-            Object {
-              "attendeeID": "5fb288d374eae06375dd75cc",
-              "audioDevices": Array [],
-              "audioSource": "mic1",
-              "content": null,
-              "date": "1623061703484",
-              "displayName": "[UnitTest]",
-              "error": "String",
-              "id": "60bdf4c70b1d2a96a99b125a",
-              "ip": "127.0.0.1",
-              "old": null,
-              "peerID": "sadg4e3geg",
-              "platform": Object {
-                "browserName": "Safari",
-                "browserVersion": "57",
-                "osName": "iOS",
-                "osVersion": "11",
-                "userAgent": "sdsdgasdg",
-              },
-              "reason": "String",
-              "roomNumber": "5fb288c7d890f45a581823e4",
-              "type": "[UnitTest] CONNECTION",
-              "videoDevices": Array [],
-              "videoSource": "cam1",
-            }
-          `);
-				});
+			const promise = back('Events_addEvent.json').then(({ nockDone }) => shutter.events.create(event).finally(nockDone));
+
+			return expect(promise).resolves.toMatchInlineSnapshot(`
+                Object {
+                  "attendeeID": "5fb288d374eae06375dd75cc",
+                  "audioDevices": Array [],
+                  "audioSource": "mic1",
+                  "content": null,
+                  "date": "1623061703484",
+                  "displayName": "[UnitTest]",
+                  "error": "String",
+                  "id": "60bdf4c70b1d2a96a99b125a",
+                  "ip": "127.0.0.1",
+                  "old": null,
+                  "peerID": "sadg4e3geg",
+                  "platform": Object {
+                    "browserName": "Safari",
+                    "browserVersion": "57",
+                    "osName": "iOS",
+                    "osVersion": "11",
+                    "userAgent": "sdsdgasdg",
+                  },
+                  "reason": "String",
+                  "roomNumber": "5fb288c7d890f45a581823e4",
+                  "type": "[UnitTest] CONNECTION",
+                  "videoDevices": Array [],
+                  "videoSource": "cam1",
+                }
+              `);
 		});
 	});
 
 	describe('Me', () => {
-		test('get', () => back('Me_get.json')
-			.then(({ nockDone }) => shutter.users.me().finally(nockDone))
-			.then(response => {
-				expect(response).toMatchInlineSnapshot(`
-            Object {
-              "created": "1605532553161",
-              "email": "example@mail.com",
-              "id": "5fb27b894c58a533f4f04aff",
-              "language": "en",
-              "name": "shutter",
-              "organizations": Array [],
-              "state": "CONFIRMED",
-              "surname": "soup",
-              "type": "SOUP",
-            }
-          `);
-			}));
+		test('get', () => {
+			const promise = back('Me_get.json').then(({ nockDone }) => shutter.users.me().finally(nockDone));
+
+			return expect(promise).resolves.toMatchInlineSnapshot(`
+                Object {
+                  "created": "1605532553161",
+                  "email": "example@mail.com",
+                  "id": "5fb27b894c58a533f4f04aff",
+                  "language": "en",
+                  "name": "shutter",
+                  "organizations": Array [],
+                  "state": "CONFIRMED",
+                  "surname": "soup",
+                  "type": "SOUP",
+                }
+              `);
+		});
 	});
 });
